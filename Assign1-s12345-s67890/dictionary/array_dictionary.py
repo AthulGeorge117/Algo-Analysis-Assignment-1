22774baa2dc756481a7c503acbb97254f195a0c7
AthulGeorge117 <89827552+AthulGeorge117@users.noreply.github.com>
2022-08-09 16:35:59 +1000

    Update array_dictionary.py

--- Assign1-s12345-s67890/dictionary/array_dictionary.py
+++ Assign1-s12345-s67890/dictionary/array_dictionary.py
@@ -15,7 +15,7 @@ class ArrayDictionary(BaseDictionary):
 
     def __init__(self):
         # TO BE IMPLEMENTED
-        pass
+        self.array = []
 
 
     def build_dictionary(self, words_frequencies: [WordFrequency]):
@@ -24,6 +24,8 @@ class ArrayDictionary(BaseDictionary):
         @param words_frequencies: list of (word, frequency) to be stored
         """
         # TO BE IMPLEMENTED
+        for word in words_frequencies:
+            self.add_word_frequency(word)
 
 
     def search(self, word: str) -> int:
@@ -34,7 +36,17 @@ class ArrayDictionary(BaseDictionary):
         """
         # TO BE IMPLEMENTED
 
+        for my_word, word_frq in self.array:
+            if my_word == word:
+                return word_frq
         return 0
+
+        # try:
+        #     index_of_word = [word_frq[0] for word_frq in self.array].index(word)
+        #     return self.array[index_of_word][1]
+        # except ValueError:
+        #     return 0
+
 
     def add_word_frequency(self, word_frequency: WordFrequency) -> bool:
         """
@@ -44,7 +56,22 @@ class ArrayDictionary(BaseDictionary):
         """
         # TO BE IMPLEMENTED
 
-        return False
+        word, word_frq = word_frequency.word, word_frequency.frequency
+
+        word_frq_tuple = (word, word_frq)
+
+        if len(self.array) == 0:
+            self.array.append(word_frq_tuple)
+            return True
+        else:
+            for index in range(len(self.array)):
+                # if word is in array
+                if self.array[index][0] == word:
+                    return False
+                else:
+                    self.array.append(word_frq_tuple)
+                    return True
+
 
     def delete_word(self, word: str) -> bool:
         """
@@ -55,6 +82,10 @@ class ArrayDictionary(BaseDictionary):
         # find the position of 'word' in the list, if exists, will be at idx-1
         # TO BE IMPLEMENTED
 
+        for index, word_frq_tuple in enumerate(self.array):
+            if word_frq_tuple[0] == word:
+                self.array.pop(index)
+                return True
         return False
 
 
@@ -64,4 +95,18 @@ class ArrayDictionary(BaseDictionary):
         @param prefix_word: word to be autocompleted
         @return: a list (could be empty) of (at most) 3 most-frequent words with prefix 'prefix_word'
         """
-        return []
+        
+        most_frq_words = []
+        for word, word_frq in self.array:
+            if word[0:len(prefix_word)] == prefix_word:
+                most_frq_words.append((word, word_frq))
+                # sorting most_frq_words list using the frq of each word in reverse order
+                most_frq_words.sort(key=lambda word_frq_tuple: word_frq_tuple[1], reverse=True)
+
+        most_frq_words = most_frq_words[:3]
+
+        return_lst = []
+        for word, word_frq in most_frq_words:
+            return_lst.append(WordFrequency(word, word_frq))
+
+        return return_lst
